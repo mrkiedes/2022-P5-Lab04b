d29efbca0a76abbdce92e46fb13ab96c578056c3
Mr. Kiedes <akiedes@vtsd.com>
2022-10-13 08:53:29 -0400

    Moveable/Resizeable Cube Code Upload

--- Lab04bvst.java
+++ Lab04bvst.java
@@ -12,8 +12,27 @@ public class Lab04bvst extends Applet
 
     public void paint(Graphics g)
     {
-        // DRAW CUBE
+        int xCoord = 100;
+        int yCoord = 100;
+        int width = 150;
+        int height = 150;
+        int diff = 50;
 
+        // DRAW CUBE
+        g.drawRect(xCoord,yCoord,width,height);
+        g.drawRect(xCoord+diff,yCoord+diff,width,height);
+
+        // Top Left
+        g.drawLine(xCoord,yCoord, xCoord+diff, yCoord+diff);
+
+        // Top Right
+        g.drawLine(xCoord+width,yCoord, xCoord+diff+width, yCoord+diff);
+
+        // Bottom Left
+        g.drawLine(xCoord,yCoord+height, xCoord+diff, yCoord+diff+height);
+
+        // Bottom Right
+        g.drawLine(xCoord+width,yCoord+width, xCoord+diff+width, yCoord+diff+width);
 
 
         // DRAW SPHERE
