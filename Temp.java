503ec7d7625e805246ac375bfac3a195171aabbf
Mr. Kiedes <akiedes@vtsd.com>
2022-10-25 13:33:20 -0400

    Pre Condition Update

---
+++ Temp.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Temp {
+
+    public static void main(String args[]) {
+
+
+    Scanner input = new Scanner(System.in);
+    int pin = 5678;
+        while(pin !=5678)
+
+    {
+        System.out.print("Enter pin. ===>>");
+        pin = input.nextInt();
+    }
+
+        System.out.println();
+        System.out.println("Program Finished");
+
+}
+}
